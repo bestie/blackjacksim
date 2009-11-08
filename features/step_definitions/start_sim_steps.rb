8d0fb721573acc24707f836758bac19338263298
Stephen Best <bestie@gmail.com>
2009-11-08 03:20:43 +0000

    abandoned cucumber for now, RSpec needed

--- features/step_definitions/start_sim_steps.rb
+++ features/step_definitions/start_sim_steps.rb
@@ -3,16 +3,75 @@ def messenger
   @messenger ||= StringIO.new
 end
 
+def dealer
+  @dealer ||= Blackjack::Dealer.new
+end
+
+Then /^I should see "(.+)"$/ do |message|
+  @messenger.string.split("\n").should include(message)
+end
+
+def strategy_file(filename)
+  filename += '.csv' unless filename[-4..-1] == '.csv'
+  File.join(File.dirname(__FILE__),'..','..','spec',"strategies",filename)
+end
+
 Given /^I have a strategy$/ do
   @strategy = Blackjack::Strategy.new(strategy_file("always_stand"))
 end
 
-When /^I start the simulator$/ do
-  bankroll = 100.00
-  @sim = Blackjack::Simulator.new(bankroll, @strategy, messenger)
-  @sim.start
+Given /^a player$/ do
+  @player = Blackjack::Player.new(@strategy, @bank_roll)
 end
 
-Then /^I should see the results$/ do
-  messenger.string.should match(/Result: ([0-9]+\.[0-9]{2})/)
+Given /^a bankroll of ([0-9]+)$/ do |bankroll_amount|
+  @bank_roll = bankroll_amount.to_i
 end
+
+Given /^the shoe has ([0-9]+) decks?$/ do |number_of_decks|
+  @shoe = Blackjack::Shoe.new(number_of_decks.to_i)
+end
+
+When /^I start the a new hand$/ do
+  @simulator = Blackjack::Simulator.new(@player, dealer, @shoe, messenger)
+  @simulator.begin_hand
+  
+  
+end
+
+Given /^the player is bankrupt$/ do
+  @player.should_receive(:bankrupt?).once.and_return(true)
+end
+
+Then /^([0-9]+) cards should be dealt$/ do |number|
+  @shoe.should_receive(:deal).exactly(number).times
+end
+
+Then /^the player should have placed a bet$/ do
+  @player.bank_roll
+  @bank_roll
+  @player.bank_roll < @bank_roll
+end
+
+Then /^the player should have ([0-9]+) cards$/ do |number|
+  @player.hand.count.should == number.to_i
+end
+
+Then /^the dealer should have ([0-9]+) cards$/ do |number|
+  dealer.hand.count.should == number.to_i
+end
+
+Given /^the player will hit ([0-9]+) times? and stand$/ do |number|
+  hits = 0
+  @player.should_receive(:decision).and_return do
+    # hits += 1
+    # return 'H' if hits <= number 
+  end
+
+  @simulator.continue
+end
+
+When /^the game continues$/ do
+  
+end
+
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,7 @@
 Dir["#{File.dirname(__FILE__)}/../../lib/**/*.rb"].each {|f| require f}
+
+require 'spec'
+require 'spec/mocks'
+require 'spec/stubs/cucumber'
+
+# require File.join(File.dirname(__FILE__),'..', '..', 'spec','spec_helper')
