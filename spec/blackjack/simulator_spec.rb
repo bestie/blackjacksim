9664a4cc241ae074f8fea695539512c50c35c369
Stephen Best <bestie@gmail.com>
2009-11-08 14:10:14 +0000

    refactored some of those test helper methods

--- spec/blackjack/simulator_spec.rb
+++ spec/blackjack/simulator_spec.rb
@@ -6,55 +6,6 @@ module Blackjack
     def output_should_include(string)
       @messenger.string.split("\n").should include(string)
     end
-
-    def player_should_decide(*decisions)
-      decisions.reverse!
-      @player.should_receive(:decision).exactly(decisions.length).times.with(@dealer.upcard).and_return{
-        decisions.pop
-      }
-    end
-    
-    def shoe_should_bust_player_in(hits)
-      if hits == 1
-        cards = [ 8, 8, 4, 4]
-        cards.push(10) # giving 22 on first hit
-      else
-        cards = [ 2, 2, 3, 3]      
-        allowance = 21 - 5
-        average_per_hit = (allowance / hits.to_f).ceil
-        (hits - 1).times do
-          cards.push(average_per_hit)
-        end
-        cards.push(average_per_hit + 1)
-      end
-      shoe_should_deal cards
-    end
-    
-    def shoe_should_deal(*cards)
-      # p cards
-      cards.flatten!.reverse!
-      @shoe.should_receive(:deal).at_least(cards.length).times.and_return { cards.pop }
-    end
-    
-    def shoe_should_deal_to(hash)
-      player_cards = hash[:player].flatten.reverse
-      dealer_cards = [10,7]
-      dealer_cards = hash[:dealer].flatten.reverse if hash[:dealer].is_a?(Array)
-      all_cards = []
-      
-      # alternate cards first two
-      2.times do
-        all_cards << player_cards.pop if player_cards.length > 0
-        all_cards << dealer_cards.pop if dealer_cards.length > 0
-      end
-      
-      # players cards next
-      player_cards.length.times { all_cards << player_cards.pop }
-      # then dealers cards
-      dealer_cards.length.times { all_cards << dealer_cards.pop }
-      
-      shoe_should_deal all_cards
-    end
     
     before(:each) do
       strategy = Strategy.new(Blackjack.strategy_file("hit_straight_under_17"))
@@ -84,8 +35,8 @@ module Blackjack
      end
      
      it "should deal no more cards if the player decides to stand" do
-       player_should_decide('S')
-       shoe_should_deal_to( :player => ['Q', 3], :dealer => [4,7,8])
+       @player.should_decide('S')
+       @shoe.should_deal_to( :player => ['Q', 3], :dealer => [4,7,8])
      
        @sim.play_hand
      
@@ -94,8 +45,8 @@ module Blackjack
      end
     
     it "should hit player once" do
-      shoe_should_deal_to( :player => [5,6,8] )
-      player_should_decide('H', 'S')
+      @shoe.should_deal_to( :player => [5,6,8] )
+      @player.should_decide('H', 'S')
       
       @sim.play_hand
       @player.hand
@@ -103,8 +54,8 @@ module Blackjack
     end
     
     it "should hit player twice" do
-      shoe_should_deal_to( :player => [5,6,4,3] )
-      player_should_decide('H', 'H', 'S')
+      @shoe.should_deal_to( :player => [5,6,4,3] )
+      @player.should_decide('H', 'H', 'S')
       
       @sim.play_hand
       @player.hand.should have(4).cards
@@ -112,20 +63,20 @@ module Blackjack
     
     it "should stop hitting the player when bust" do
       @player.should_receive(:decision).at_least(:once).and_return('H')
-      shoe_should_bust_player_in(2)
+      @shoe.should_bust_player_in(2)
       @sim.play_hand
     end
     
     it "should pay the player 3 to 2 if she gets blackjack" do
       lambda {
         @player.should_not_receive(:decision)
-        shoe_should_deal_to( :player => ['A', 'K'])
+        @shoe.should_deal_to( :player => ['A', 'K'])
         @sim.play_hand
       }.should change(@player, :bank_roll).by(1.5)
     end
     
     it "should not deal to dealer if dealer already has 17" do
-      shoe_should_deal_to( :player => [10,10], :dealer => [10,7] )
+      @shoe.should_deal_to( :player => [10,10], :dealer => [10,7] )
       @player.should_receive(:decision).once.and_return('S')
       @sim.play_hand
     end
@@ -133,8 +84,8 @@ module Blackjack
     it "should test its own test helper" do
       player_cards = [10,7,3]
       dealer_cards = [2,5,2,6,3]
-      shoe_should_deal_to( :player => player_cards, :dealer => dealer_cards )
-      player_should_decide('H', 'S')
+      @shoe.should_deal_to( :player => player_cards, :dealer => dealer_cards )
+      @player.should_decide('H', 'S')
       @sim.play_hand
       
       @player.hand.cards.should == player_cards
@@ -143,30 +94,30 @@ module Blackjack
     end
     
     it "should give dealer cards until she reaches 17" do
-      shoe_should_deal_to( :player => [10,7], :dealer => [2,5,2,6,3] )
-      player_should_decide('S')
+      @shoe.should_deal_to( :player => [10,7], :dealer => [2,5,2,6,3] )
+      @player.should_decide('S')
       @sim.play_hand
     end
     
     it "should pay the player should he have a better hand" do
-      shoe_should_deal_to( :player => ['A',9], :dealer => [2,5,2,6,3] )
-      player_should_decide('S')
+      @shoe.should_deal_to( :player => ['A',9], :dealer => [2,5,2,6,3] )
+      @player.should_decide('S')
       lambda {
         @sim.play_hand
       }.should change(@player, :bank_roll).by(1)
     end
     
     it "should pay the player if the dealer busts" do
-      shoe_should_deal_to( :player => [2,3], :dealer => [2,5,2,6,8] )
-      player_should_decide('S')
+      @shoe.should_deal_to( :player => [2,3], :dealer => [2,5,2,6,8] )
+      @player.should_decide('S')
       lambda {
         @sim.play_hand
       }.should change(@player, :bank_roll).by(1)
     end
     
     it "should take the players money and end the game if the player loses" do
-      shoe_should_deal_to( :player => ['Q', 4, 3], :dealer => [2,5,2,6,5] )
-      player_should_decide('H', 'S')
+      @shoe.should_deal_to( :player => ['Q', 4, 3], :dealer => [2,5,2,6,5] )
+      @player.should_decide('H', 'S')
 
       lambda {
         @sim.play_hand
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,59 @@ module Blackjack
     end
   end
   
+  class Player
+    def should_decide(*decisions)
+      decisions.reverse!
+      self.should_receive(:decision).exactly(decisions.length).times.and_return{
+        decisions.pop
+      }
+    end
+  end
+  
+  class Shoe
+    def should_deal(*cards)
+      # p cards
+      cards.flatten!.reverse!
+      self.should_receive(:deal).at_least(cards.length).times.and_return { cards.pop }
+    end
+    
+    def should_bust_player_in(hits)
+      if hits == 1
+        cards = [ 8, 8, 4, 4]
+        cards.push(10) # giving 22 on player's first hit
+      else
+        cards = [ 2, 2, 3, 3] # deal a minimal hand for sufficient headroom
+        allowance = 21 - 5
+        average_per_hit = (allowance / hits.to_f).ceil
+        (hits - 1).times do
+          cards.push(average_per_hit)
+        end
+        cards.push(average_per_hit + 1)
+      end
+      should_deal cards
+    end
+    
+    def should_deal_to(hash)
+      player_cards = hash[:player].flatten.reverse
+      dealer_cards = [10,7]
+      dealer_cards = hash[:dealer].flatten.reverse if hash[:dealer].is_a?(Array)
+      all_cards = []
+      
+      # alternate cards first two
+      2.times do
+        all_cards << player_cards.pop if player_cards.length > 0
+        all_cards << dealer_cards.pop if dealer_cards.length > 0
+      end
+      
+      # players cards next
+      player_cards.length.times { all_cards << player_cards.pop }
+      # then dealers cards
+      dealer_cards.length.times { all_cards << dealer_cards.pop }
+      
+      should_deal all_cards
+    end
+  end
+  
   def self.strategy_file(filename)
     filename += '.csv' unless filename[-4..-1] == '.csv'
     File.join(File.dirname(__FILE__),"strategies",filename)
