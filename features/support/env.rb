54cee882c006f2940044297984344709f1bf5613
Stephen Best <bestie@gmail.com>
2009-11-08 03:21:55 +0000

    this is required for running cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,5 +3,3 @@ Dir["#{File.dirname(__FILE__)}/../../lib/**/*.rb"].each {|f| require f}
 require 'spec'
 require 'spec/mocks'
 require 'spec/stubs/cucumber'
-
-# require File.join(File.dirname(__FILE__),'..', '..', 'spec','spec_helper')
